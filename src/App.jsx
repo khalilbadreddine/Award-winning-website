9c5af89ad7998c7c14a71ee1f3cfcc2df9105f5e
khalilbadreddine <khalilbadre5@gmail.com>
2024-12-01 15:02:12 +0100

    Adding Assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <h1 className="text-3xl text-orange-400 font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
