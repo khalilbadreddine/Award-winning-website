b62871871b9e4314d8fc2f3043776f7596d81a91
khalilbadreddine <khalilbadre5@gmail.com>
2024-12-03 15:42:29 +0100

    Complete Hero section with gsap Animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Hero from "./Components/Hero"
+import Hero from "./Components/Hero";
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Hero/>
+      <Hero />
+      <section className="z-0 min-h-screen bg-blue-500" />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
